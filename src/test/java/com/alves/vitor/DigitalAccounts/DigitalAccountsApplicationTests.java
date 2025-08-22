0def00629b5556ee865bf0536c7117a6ece8b54b
Vitor-Alvess <20vitor11@gmail.com>
2025-08-21 22:59:02 -0300

    Commit inicial do projeto

---
+++ src/test/java/com/alves/vitor/DigitalAccounts/DigitalAccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alves.vitor.DigitalAccounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalAccountsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
